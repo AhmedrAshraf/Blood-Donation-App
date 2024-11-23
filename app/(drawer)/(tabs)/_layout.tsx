c97871245e4c8fab94d44ce36207b80be85a26c9
saadihehe <msaad0900@gmail.com>
2024-11-23 15:36:01 +0500

    .

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 35,
-    backgroundColor: '#FF5252', // Main color for the floating button
+    backgroundColor: '#ed631a', // Main color for the floating button
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 40, // Lift the button slightly above the tab bar
